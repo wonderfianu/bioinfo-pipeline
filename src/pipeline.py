d0faba20a63a0083c0d3f4e93890e415641438b7
Wonder <31032931@sun.ac.za>
2026-03-29 22:41:18 +0200

    Quality Control steps

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,13 @@
+import numpy as np 
 
+#Quality control 
+def quality_control(seq_data)
+	print('The quality control begins')
+# A simple simultion
+	with open(file_path, 'r') as file: 
+	for line in file:
+	    if len(line) > 10:
+		print(f"this is a low quality read:", line)
+	  print(f"QC completed")
+file = ['ATC', 'TTAC', 'TAGC']
+quality_contol(file)
